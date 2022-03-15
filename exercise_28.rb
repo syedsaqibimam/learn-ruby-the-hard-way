d2f3e739728996bb71f7760be4b66e328da4199f
syedsaqibimam <syedsaqibimam@hotmail.co.uk>
2022-03-15 01:11:29 +0000

    What If

---
+++ exercise_28.rb
@@ -0,0 +1,37 @@
+true && true
+
+false && false
+
+1 == 1 && 2 ==1
+
+"test" == "test"
+
+1 ==1 || 2 !=1
+
+true && 1==1
+
+false && 0 != 0
+
+true || 1 == 1
+
+true || 1 == 1
+
+"test" == "testing"
+
+1 != 0 && 2 == 1
+
+"test" != "testing"
+
+"test" == 1
+
+!(true && false)
+
+!(1 == 1 && 0 != 1)
+
+!("testing" == "testing" && "Zed" == "Cool Guy")
+
+1 == 1 && (!("testing" == 1 || 1 == 0))
+
+"chunky" == "bacon" && (!(3 == 4 || 3 == 3))
+
+3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))
